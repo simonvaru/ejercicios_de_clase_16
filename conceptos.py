bb6e152c1c110fd79d6c27399975a92fc5fb704f
simonvaru <pennywise1540b@hotmail.com>
2022-09-23 18:42:01 -0300

    Agrego conceptos.py.

---
+++ conceptos.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+
+print("=======================")
+dt = datetime.now()
+print(dt)
+
+print(dt.year)
+print(dt.month)
+print(dt.day)
+print(dt.hour)
+print(dt.minute)
+print(dt.second)
